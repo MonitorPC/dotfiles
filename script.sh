82bc53927bc21207055017681835670f5c8f2d72
Your Name <you@example.com>
2022-11-18 04:05:55 +0000

    wow

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@ rm -rf ~/.config/bspwm
 rm -rf ~/.config/sxhkd
 rm -rf ~/.config/fish
 rm -rf ~/.config/nvim
-rm -rf ~/.config/alacritty
+rm -rf ~/.config/kitty
 rm -rf ~/.config/picom
 
-stow bspwm sxhkd fish nvim alacritty picom
+stow bspwm sxhkd fish nvim picom misc kitty
