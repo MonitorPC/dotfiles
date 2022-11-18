b39dcadece1cc38a203ba7ce83105194caa643f1
Your Name <you@example.com>
2022-11-18 07:47:33 +0000

    wow

--- script.sh
+++ script.sh
@@ -4,5 +4,6 @@ rm -rf ~/.config/fish
 rm -rf ~/.config/nvim
 rm -rf ~/.config/kitty
 rm -rf ~/.config/picom
+rm -rf ~/riscv/rars_27a7c1f.jar
 
-stow bspwm sxhkd fish nvim picom misc kitty
+stow bspwm sxhkd fish nvim picom misc kitty riscv
